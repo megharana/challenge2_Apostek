7d6133df97768827e1da46cce02f107ff0842559
megharana <megharanait95@gmail.com>
2018-08-29 22:51:19 +0530

    added comments

--- ChallengeApostek.java
+++ ChallengeApostek.java
@@ -17,26 +17,26 @@ class ChallengeApostek{
 		ca.special_sum(A,N);
 		s1.close();
 	}
-	public void special_sum(int A[], int N){
+	public void special_sum(int A[], int N){                   //to calculate the special sum
 		int j=0,sum1,sum2=0;
-		List<Integer> specialSum = new ArrayList<Integer>();
-		for(int index = 0; index < N; index++){
-			for (int i=index,k=1;i<N;i++,k++){
-				if(i>index)
+		List<Integer> specialSum = new ArrayList<Integer>();   //list to add up all special sum of each index
+		for(int index = 0; index < N; index++){					//INDEX provided
+			for (int i=index,k=1;i<N;i++,k++){					//loop that breaks it in modules (A[1])+(A[2]+A[3]) 
+				if(i>index)										//'i' takes the updated 'j' value after completion of inner loop
 					i=j;
-				if(i+k<=N){
+				if(i+k<=N){										//checks the possibility of breaking it in module
 					sum1=0;
-					for(j=i;j<i+k;j++ ){
-						sum1+=A[j];
+					for(j=i;j<i+k;j++ ){						//loop for each module
+						sum1+=A[j];								//sum of each module i.e. (A[2]+A[3])
 						System.out.print(A[j]+"\t");
 					}
-					System.out.println("The SUM "+sum1);
-					sum2+=sum1;
+					System.out.println("The SUM "+sum1);		
+					sum2+=sum1;									//takes the sum of each modules 
 				}
 				System.out.print("\n");
 			}
-			System.out.println("The total Sum "+sum2);
-			specialSum.add(sum2);
+			System.out.println("The total Sum "+sum2);			// printing the sum of each index
+			specialSum.add(sum2);								//adding up all the sum of indexes in list 
 			System.out.println(sum2);
 		}
 	}
