32b2682a05b370dc5e0bf01d158c708f7a9219b5
MEGHA RANA <megharanait95@gmail.com>
2018-09-04 12:40:52 +0000

    Implementation of maximum vacant sequence- DONE
    Able to fetch count of maximum vacant squence together with starting and end index

--- challengeApostek3.java
+++ challengeApostek3.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 //for scanner
 import java.util.*;
 
-class Test{
+class challengeApostek3{
 	public static void main(String[] args) throws Exception {
 		
 		Scanner s1 = new Scanner(System.in);
@@ -13,46 +13,46 @@ class Test{
 		for(int i=0;i<noOfChairs;i++){
 			chair[i] = -1;
 		}
-		chair[2]=1;
+		chair[3]=1;
 		// System.out.println("Enter no.of person");
 		// int noOfPerson = s1.nextInt();
 		// char preferences[] = new char[noOfPerson];
 		// System.out.println("Enter the preferences person wise from 1-"+noOfPerson);
 		// int noOfPerson = s1.nextChar();
-		int tempIndexes[] = new int[noOfChairs]; 
-		int k=0,sum=1;
+		//int tempIndexes[] = new int[noOfChairs];
+		
+		List<Integer> tempIndexes = new ArrayList<Integer>();
+		int k=0,sum=0;
 		for(int j=0;j<noOfChairs;j++){
-			if(chair[j]==-1){
-				tempIndexes[k]=j;
-			}
-			k++;
+			if(chair[j]== -1){				
+				tempIndexes.add(j);				
+			}			
 		}
-		List<Integer> sums = new ArrayList<Integer>();
-		k=tempIndexes[0];
-		for(int j=1;j<tempIndexes.length;j++){		
-			k++;
-			System.out.println("The sum b4 if condition" + sum +" "+k +" "+tempIndexes[j]);
-			if(k!=tempIndexes[j]){
-				k=tempIndexes[j];
-				sums.add(sum);
+		System.out.println("Indexes have -1");
+		System.out.println(tempIndexes);		
+		HashMap<Integer, Integer> indexSum = new HashMap<>(); // hashmap for saving the last index and count of continuous vacant chairs
+		k=tempIndexes.get(0);		
+		for(int tempIndex : tempIndexes){		
+			if(k!=tempIndex){
+				indexSum.put(k-1,sum);
+				k=tempIndex;
 				sum=1;
 			}
-			else if(k==tempIndexes[j]){
+			else if(k==tempIndex){
 				sum++;
 			}
-			
-			System.out.println("The sum inside loop" + sum +" "+k +" "+tempIndexes[j]);
+			if(k==(chair.length-1))
+				indexSum.put(k,sum);
+			k++;
 
-			if(k==tempIndexes.length-1)
-				sums.add(sum);
-
+		}	
+		System.out.println(indexSum);
+		Iterator<Map.Entry<Integer, Integer>> entries = indexSum.entrySet().iterator();
+		while (entries.hasNext()) {
+			Map.Entry<Integer, Integer> entry = entries.next();
+			System.out.println("startIndex = "+((entry.getKey()+1)-entry.getValue())+", endIndex = " + entry.getKey() +", Count = " + entry.getValue()); //printing the starting index and last index of maximum continuous vacant sequence of chairs
 		}
-
-		System.out.println(sums);
-		for(int i=0;i<noOfChairs;i++){
-			System.out.println(chair[i]); 
-		}
-
+		
 	}
 }
 
